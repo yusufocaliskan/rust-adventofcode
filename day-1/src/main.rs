3caaff0f946cdf2c30a4a81ef451470153f544c3
yusufocaliskan <yusufocaliskan@gmail.com>
2024-10-08 11:18:39 +0300

    clean-up

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,15 +1,56 @@
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{BufRead, BufReader};
 
-fn main() -> io::Result<()> {
-    let file = File::open("src/inputs.txt").expect("No File-->");
-    let reader = BufReader::new(file);
+fn main() {
+    //finds fist and last digits in a line
+    //and calculate all lines
+    part_one();
+
+    //
+}
+
+fn part_one() {
     let mut line_total = 0;
+    let reader = get_file_content("src/inputs.txt".to_string());
+    for line_result in reader.lines() {
+        let line = match line_result {
+            Ok(line) => line,
+            Err(_) => continue,
+        };
+
+        //get the digits.
+        let digits: Vec<char> = line.chars().filter(|ch| ch.is_numeric()).collect();
+        print!("{:?}", digits);
+
+        //find the first and last digit
+        let (first, last) = match (digits.first(), digits.last()) {
+            (Some(first), Some(last)) => (first, last),
+            _ => continue,
+        };
+
+        let num_string: String = format! {"{}{}", first, last};
+
+        if let Ok(num) = num_string.parse::<i32>() {
+            line_total += num;
+        }
+    }
+
+    println!("The answer of the part ONE --> {:?}", line_total);
+}
+
+fn part_two() {
+    let mut line_total = 0;
+
+    let alphabetic_nums = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let reader = get_file_content("src/inputs.txt".to_string());
+
     for line_result in reader.lines() {
         if let Ok(line) = line_result {
             //get the digits.
             let digits: Vec<char> = line.chars().filter(|ch| ch.is_numeric()).collect();
-            print!("{:?}", digits);
+            // print!("{:?}", digits);
 
             //find the first and last digit
             if let (Some(first), Some(last)) = (digits.first(), digits.last()) {
@@ -22,6 +63,12 @@ fn main() -> io::Result<()> {
         }
     }
 
-    println!("Right Answer Is =-> {:?}", line_total);
-    Ok(())
+    println!("The answer of the part TWO --> {:?}", line_total);
+}
+
+/// returns content of the give file
+fn get_file_content(file_path: String) -> BufReader<File> {
+    let file = File::open(file_path).expect("No File-->");
+    let reader = BufReader::new(file);
+    return reader;
 }
